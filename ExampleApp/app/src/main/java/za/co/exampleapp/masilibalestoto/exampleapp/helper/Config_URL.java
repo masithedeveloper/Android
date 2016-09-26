62a44558c30ac43531a3abe1c131c4c3c53c367f
masithedeveloper <masithedeveloper@gmail.com>
2016-09-26 10:35:17 +0200

    API URL
    changes api url

--- ExampleApp/app/src/main/java/za/co/exampleapp/masilibalestoto/exampleapp/helper/Config_URL.java
+++ ExampleApp/app/src/main/java/za/co/exampleapp/masilibalestoto/exampleapp/helper/Config_URL.java
@@ -4,11 +4,12 @@ package za.co.exampleapp.masilibalestoto.exampleapp.helper;
 
 public class Config_URL
 {
-	private static String base_URL = "http://192.168.32.186:8080/";	 // Wayne if you see this comment, change this to point to your host server IP
+	//private static String base_URL = "http://192.168.32.186:8080/";	 // Wayne if you see this comment, change this to point to your host server IP
+	private static String base_URL = "http://globalstandardrating.com/demo/";	 // Wayne if you see this comment, change this to point to your host server IP
 	// Server user login url
-	public static String URL_LOGIN = base_URL+"web_api/";
+	public static String URL_LOGIN = base_URL+"";
 	// Server user register url
-	public static String URL_REGISTER = base_URL+"web_api/";
+	public static String URL_REGISTER = base_URL+"";
 	// Server values upload url
-	public static String URL_UPLOAD_VALUES = base_URL+"web_api/";
+	public static String URL_UPLOAD_VALUES = base_URL+"";
 }
